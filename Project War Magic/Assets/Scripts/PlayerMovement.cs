fed1435be32ee11b9bdbb796930570bbe88adb98
KalvinMalloch <km230683@falmouth.ac.uk>
2020-06-16 17:18:32 +0100

    Created project and added simple player movement via clicking.

---
+++ Project War Magic/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LayerMask clickable;
+
+    private NavMeshAgent playerAgent;
+
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast (mouseRay, out hitInfo, 100, clickable))
+            {
+                playerAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
